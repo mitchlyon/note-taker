68bef8f714faa0893faa471f57c2371f1e6ba0f8
Mitchell Lyon <kekoa.lyon@gmail.com>
2021-10-10 16:36:22 -0600

    needs some work

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# note-taker
+# Note Taker
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+
+## Description
+
+A web based app that allows a user to take and save notes.
+
+## Table of contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Instalation
+
+to install dependencies, run the following:
+
+`
+npm i
+`
+
+## Usage
+
+
+
+## License
+
+This repository is licensed under the MIT license.
+
+## Contributing
+
+
+
+## Tests
+
+To run tests, run the following:
+
+`
+npm run test
+`
+
+## Questions
+
+Questions about this repository? Please contact me at[kekoa.lyon@gmail.com](mailto:kekoa.lyon@gmail.com).
+View more of my work in GitHub at [mitchlyon](https://github.com/mitchlyon)
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const uuid = require("uuid");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/notes", (req, res) => {
+    res.sendfile(path.join(__dirname, "./db/db.json"))
+});
+
+app.post("api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./assets/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./assets/notes.html"));
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
